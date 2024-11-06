eb56a2bc5524b0384b315ab88e5b4667a8cf1bdb
SKSritharan <sk.sritharan75@gmail.com>
2024-11-06 22:45:55 +0530

    Added documentation with swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configure Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Submit Jokes API')
+    .setDescription('API for submitting jokes')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // Start the application
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
